9c1dae18b828b24126a23fd3ebdc96fee75bf364
eakkasitgithub <eakkasit@gmail.com>
2025-02-01 17:17:52 +0700

    auto-commit

---
+++ src/App HELLO.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Hello World xxxf</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+  text-align: center;
+  margin-top: 40px;
+}
+</style>
