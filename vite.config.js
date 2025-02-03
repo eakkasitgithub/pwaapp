07bfbd1b0eb7d8a0d5c2ad95cd83e7f1c2256792
eakkasitgithub <eakkasit@gmail.com>
2025-02-03 13:06:21 +0700

    auto-commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
-
-const path = require('path')
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    // Vuetify plugin configuration
     vuetify({
       autoImport: true,
     }),
@@ -17,19 +16,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      'leaflet': 'leaflet/dist/leaflet.js' // Ensure Vite resolves Leaflet correctly
     },
-  },
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
-})
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'] // Allow auto-resolving file extensions
+  }
+});
